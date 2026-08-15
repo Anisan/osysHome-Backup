b3123940d2c71dc401b5d444f9064bfc78335c29
Eraser <eraser1981@gmail.com>
2026-08-15 15:42:54 +0300

    update localization

--- forms/SettingsForm.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-# Определение класса формы
-class SettingsForm(FlaskForm):
-    crontab = StringField('Crontab', validators=[DataRequired()])
-    submit = SubmitField('Submit')
